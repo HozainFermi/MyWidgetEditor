e3ceb8a694b2b749fbeb2e8473a2738711cced23
HozainFermi <ded.dedov.1901@mail.ru>
2025-12-29 17:14:13 +0400

    deleted old include files

--- MyWidgetEditor/src/include/EditorMainWindowLayout.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <imgui.h>
-#include <vector>
-#include "Widget.h"
-#include <GLFW/glfw3.h>
-
-void ShowMainWindowLayout(bool* p_open, bool* use_grid , ImGuiViewport* viewport, GLFWwindow* window, std::vector<Widget>& assets, std::vector<Widget>& used_assets, ImGuiIO& io);
