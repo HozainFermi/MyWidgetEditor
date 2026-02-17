37de4e6f41769257f405bc8088ad2471236e8978
HozainFermi <ded.dedov.1901@mail.ru>
2026-02-17 13:15:19 +0400

    EditorMainWindowLayout path

--- MyWidgetEditor/src/ui/EditorMainWindowLayout.cpp
+++ MyWidgetEditor/src/ui/EditorMainWindowLayout.cpp
@@ -22,7 +22,7 @@ void Editor::OnFileForLoadSelected(const std::string& filename) {
     widget_manager_.LoadFromFile("./configs/"+filename+".json", window_props_);                
 }
 void Editor::OnFileForRunSelected(const std::string& filename) {
-    std::filesystem::path relfilePath("../../../../MyWidgetEditor/configs/" + filename + ".json");
+    std::filesystem::path relfilePath(std::string(PROJECT_SOURCE_DIR)+"/configs/" + filename + ".json");
     std::filesystem::path relDrawer("../Drawer/Drawer.exe");
     //C:\Users\Hoz\Desktop\MyWidgetEditor\out\build\x64-debug\Drawer
     std::filesystem::path absExePath = std::filesystem::absolute(relDrawer);
